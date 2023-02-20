c55431c877b6e0b0ac53267f8ce72a8c401068a2
Paweł Bernaciak <pawelbernaciak@zohomail.eu>
2023-02-20 20:56:50 +0100

    Add station name search

--- MuseJet.GUI/ViewModels/SearchViewModel.cs
+++ MuseJet.GUI/ViewModels/SearchViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MuseJet.Common.Services;
+﻿using MuseJet.Common.Models;
+using MuseJet.Common.Services;
+using MuseJet.GUI.Commands;
 using MuseJet.GUI.Events;
 using RadioBrowser;
 using System;
@@ -9,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MuseJet.GUI.ViewModels
 {
@@ -18,6 +21,8 @@ namespace MuseJet.GUI.ViewModels
         private readonly StationService _service;
         private ObservableCollection<SearchItemViewModel> _searchResults;
         private bool _isLoading;
+        private bool _isSearching;
+        private string _searchTerm;
         private uint _count = 100;
 
         public ObservableCollection<SearchItemViewModel> SearchResults
@@ -40,12 +45,52 @@ namespace MuseJet.GUI.ViewModels
             }
         }
 
+        public bool IsSearching
+        {
+            get => _isSearching;
+            set
+            {
+                _isSearching = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SearchTerm
+        {
+            get => _searchTerm;
+            set
+            {
+                _searchTerm = value;
+                OnPropertyChanged();
+            }
+        }
+
         public SearchViewModel(RadioBrowserClient browser, StationService service)
         {
             _browser = browser;
             _service = service;
+            _isSearching = false;
             _searchResults = new();
             OnGetToEnd();
+
+            SubmitSearchCommand = new RelayCommand(async (obj) =>
+            {
+                IsSearching = true;
+                IsLoading = true;
+                var results = await _browser.Search.ByNameAsync(SearchTerm);
+                SearchResults = new(results.Select(s => new SearchItemViewModel(_service, s)).Where(s => s.Visible));
+                IsLoading = false;
+            }, null);
+
+            BackCommand = new RelayCommand((obj) =>
+            {
+                IsLoading = true;
+                SearchResults = new();
+                SearchTerm = string.Empty;
+                IsSearching = false;
+                _count = 100;
+                OnGetToEnd();
+            }, null);
 
             _service.StationStateChanged += OnStationStateChanged;
         }
@@ -55,12 +100,18 @@ namespace MuseJet.GUI.ViewModels
             StationStateChangeEventArgs parameter = (StationStateChangeEventArgs)args;
             if (parameter.Type == ChangeType.Add)
             {
-                SearchResults.Remove(SearchResults.First(s => s.Id == parameter.ChangedStation.Id));
+                SearchItemViewModel? station = SearchResults.FirstOrDefault(s => s.Id == parameter.ChangedStation.Id);
+                if (station is null)
+                    return;
+                else
+                    SearchResults.Remove(station);
             }
         }
 
         public void OnGetToEnd()
         {
+            if (IsSearching)
+                return;
             IsLoading = true;
             Task.Run(async () =>
             {
@@ -70,5 +121,8 @@ namespace MuseJet.GUI.ViewModels
                 IsLoading = false;
             });
         }
+
+        public ICommand SubmitSearchCommand { get; set; }
+        public ICommand BackCommand { get; set; }
     }
 }
